bf3385c801626f8f8e36ad4c93631caf19986174
dam824 <damiendagory@gmail.com>
2024-04-09 00:33:05 +0200

    add useReducer hook

--- src/App.jsx
+++ src/App.jsx
@@ -4,93 +4,83 @@ import AddTodo from './component/AddTodo'
 import TodoLists from './component/TodoLists'
 import themeContext from './context/theme';
 
-import { useState } from 'react';
+{/**import react hook  */}
+import { useReducer } from 'react';
+import todoReducer from './reducers/todoReducer';
 
 function App() {
+  const [state, dispatch] = useReducer(todoReducer, {
+    theme: 'primary',
+    todoList: [],
+  })
 
-  const [todoList, setTodoList] = useState([]);
-     
-  {/** ajouter une tache  */}
+   {/**action to dispatch parameters  */}
+ 
   const addTodo = (content) => {
-    const todo = {
-      id: crypto.randomUUID(),
-      content,
-      done: false,
-      edit: false,
-      selected: false
-    }
-      setTodoList([...todoList,todo  ])
+   dispatch({
+    type: 'ADD_TODO',
+    content
+   })
   }
 
-  {/** supprimer une tache  */}
+
   const deleteTodo = (id) => {
-    setTodoList(todoList.filter(todo => todo.id !== id));
+     dispatch({
+      type:'DELETE_TODO',
+      id
+     })
   }
 
   const toggleTodo = (id) =>{
-     setTodoList(
-      todoList.map((todo) =>
-      todo.id === id
-      ? {
-          ...todo,
-          done: !todo.done
-      }
-      :todo
-      )
-     );
+      dispatch({
+        type: 'TOGGLE_TODO',
+        id,
+      })
+  }
+
+  const toggleEdit = (id) => {
+      dispatch({
+        type: 'TOGGLE_EDIT_TODO',
+        id
+      })
   }
 
  
- 
   const editTodo = (id, content) => {
-    setTodoList(todoList.map(todo => todo.id === id ? ({
-      ...todo,
-      edit: false,
-      content
-    }) : todo))
+      dispatch({
+        type: 'EDIT_TODO',
+        id,
+        content
+      })
   }
 
   const selectTask = (id) => {
-    setTodoList(
-      todoList.map((todo) => 
-      todo.id === id 
-      ? ({
-      ...todo,
-      selected:true,
-    }) : ({
-      ...todo,
-      selected:false
-    })))
+      dispatch({
+        type: 'SELECT_TODO',
+        id
+      })
   }
 
-
-  {/** editer une tache  */}
-  const toggleEdit = (id) => {
-    setTodoList(
-      todoList.map((todo)=> 
-      todo.id === id ? {
-        ...todo,
-        edit: !todo.edit
-      }
-      :todo
-      )
-    );
-  }
-
-  const [theme, setTheme] = useState('primary')
   const handleChange = (e) => {
-    setTheme(e.target.value);
-
+    dispatch({
+      type : 'SET_THEME',
+      theme: e.target.value
+    })
   }
+
+ 
+ 
+
+  
 
   return (
     
-    <themeContext.Provider value={theme}>
+    <themeContext.Provider value={state.theme}>
       <div className='d-flex flex-row justify-content-center align-items-center'>
       <div className='card container p-20'>
         <h1 className='mb-20 flex-row justify-content-center align-items-center'>
           <span className='flex-fill'>Todo List </span>
-          <select value={theme} onChange={handleChange}>
+          <select value={state.theme} onChange={handleChange}>
             <option value="primary">Orange</option>
             <option value="secondary">Bleu</option>
           </select>
@@ -98,7 +88,7 @@ function App() {
         <AddTodo addTodo={addTodo} />
         {/**on transmet la todolist a notre composant todoList */}
         <TodoLists 
-        todoList={todoList} 
+        todoList={state.todoList} 
         deleteTodo={deleteTodo} 
         toggleTodo={toggleTodo} 
         toggleEdit={toggleEdit} 
